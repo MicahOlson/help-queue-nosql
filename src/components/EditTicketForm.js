eff8105ce611374963d050d3808a90981bcbfa9a
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-04-22 10:30:47 -0700

    Add EditTicketForm component

---
+++ src/components/EditTicketForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReusableForm from './ReusableForm';
+
+function EditTicketForm(props) {
+  return (
+    <>
+      <ReusableForm buttonText="Update Ticket" />
+    </>
+  );
+}
+
+export default EditTicketForm;
