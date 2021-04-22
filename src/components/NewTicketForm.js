d87f741efeb8388f9c297db088cbb2fe432f1e68
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-04-22 09:36:56 -0700

    Incorporate ReusableForm into NewTicketForm

--- src/components/NewTicketForm.js
+++ src/components/NewTicketForm.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReusableForm from './ReusableForm';
 import { v4 } from 'uuid';
 import PropTypes from 'prop-types';
 
@@ -14,23 +15,10 @@ function NewTicketForm(props) {
   }
   return (
     <>
-      <form onSubmit={handleNewTicketFormSubmission}>
-        <input
-          type='text'
-          name='names'
-          placeholder='Pair Names'
-        />
-         <input
-          type='text'
-          name='location'
-          placeholder='Location'
-        />
-        <textarea
-          name='issue'
-          placeholder='Describe your issue.'
-        />
-        <button type='submit'>Help!</button>
-      </form>
+      <ReusableForm 
+        formSubmissionHandler={handleNewTicketFormSubmission}
+        buttonText="Help!"
+      />
     </>
   );
 }
