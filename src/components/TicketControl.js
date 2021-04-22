2b53242260ff96f303de1aecc49fd1a5046f1e73
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-04-22 08:04:36 -0700

    Add selectedTicket handler method

--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -27,6 +27,11 @@ class TicketControl extends React.Component {
     });
   }
 
+  handleChangingSelectedTicket = (id) => {
+    const selectedTicket = this.state.mainTicketList.filter(ticket => ticket.id === id)[0];
+    this.setState({selectedTicket: selectedTicket});
+  }
+
   render() {
     let currentlyVisibleState = null;
     let buttonText = null;
