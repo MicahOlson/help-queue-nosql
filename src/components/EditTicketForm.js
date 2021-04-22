2d6d81d9f6a625d6837eac3d0006d8fb400e511c
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-04-22 10:58:48 -0700

    Add event handler and capture form values

--- src/components/EditTicketForm.js
+++ src/components/EditTicketForm.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import ReusableForm from './ReusableForm';
+import PropTypes from 'prop-types';
 
 function EditTicketForm(props) {
+  const { ticket } = props;
+
+  function handleEditTicketFormSubmission(event) {
+    event.preventDefault();
+    props.onEditTicket({
+      names: event.target.names.value,
+      location: event.target.location.value,
+      issue: event.target.issue.value,
+      id: ticket.id
+    });
+  }
+
   return (
     <>
-      <ReusableForm buttonText="Update Ticket" />
+      <ReusableForm 
+        formSubmissionHandler={handleEditTicketFormSubmission} 
+        buttonText="Update Ticket"
+      />
     </>
   );
 }
+
+EditTicketForm.propTypes = {
+  ticket: PropTypes.object,
+  onEditTicket: PropTypes.func
+};
 
 export default EditTicketForm;
