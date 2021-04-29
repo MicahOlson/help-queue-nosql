dbf9fe5300b09a4a0790f53dddb3ad484cf76eba
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-04-28 20:06:46 -0700

    Add form visibility state with redux

--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -22,6 +22,12 @@ class TicketControl extends React.Component {
         selectedTicket: null,
         editing: false
       });
+    } else {
+      const { dispatch } = this.props;
+      const action = {
+        type: 'TOGGLE_FORM'
+      }
+      dispatch(action);
     }
   }
 
@@ -36,6 +42,10 @@ class TicketControl extends React.Component {
       issue: issue,
     }
     dispatch(action);
+    const action2 = {
+      type: 'TOGGLE_FORM'
+    }
+    dispatch(action2);
   }
 
   handleChangingSelectedTicket = (id) => {
