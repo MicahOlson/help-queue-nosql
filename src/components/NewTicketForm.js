a9219f40dc6ddf37ba9efec78f5f2d79642323b2
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-05-03 15:09:03 -0700

    Add useFirestore() hook
    Co-authored-by: Joseph Giunta <jgnohack@gmail.com>
    Co-authored-by: Scott O'Neil <scottieoneil@gmail.com>

--- src/components/NewTicketForm.js
+++ src/components/NewTicketForm.js
@@ -1,25 +1,29 @@
 import React from 'react';
-import Moment from 'moment';
+// import Moment from 'moment';
 import PropTypes from 'prop-types';
-import { v4 } from 'uuid';
+// import { v4 } from 'uuid';
 import ReusableForm from './ReusableForm';
+import { useFirestore } from 'react-redux-firebase'
 
 function NewTicketForm(props) {
-  function handleNewTicketFormSubmission(event) {
+  const firestore = useFirestore();
+
+  function addTicketToFirestore(event) {
     event.preventDefault();
-    props.onNewTicketCreation({
-      names: event.target.names.value,
-      location: event.target.location.value,
-      issue: event.target.issue.value,
-      id: v4(),
-      timeOpen: new Moment(),
-      formattedWaitTime: new Moment().fromNow(true)
-    });
+    props.onNewTicketCreation();
+    return firestore.collection('tickets').add(
+      {
+        names: event.target.names.value,
+        location: event.target.location.value,
+        issue: event.target.issue.value,
+        timeOpen: firestore.FieldValue.serverTimestamp()
+      }
+    )
   }
   return (
     <>
       <ReusableForm 
-        formSubmissionHandler={handleNewTicketFormSubmission}
+        formSubmissionHandler={addTicketToFirestore}
         buttonText="Help!"
       />
     </>
