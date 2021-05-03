94fa914cf6629b7e0050916e15a52c2e479b2b8d
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-05-03 15:10:31 -0700

    Incorporate firestore hook
    Co-authored-by: Joseph Giunta <jgnohack@gmail.com>
    Co-authored-by: Scott O'Neil <scottieoneil@gmail.com>

--- src/components/TicketList.js
+++ src/components/TicketList.js
@@ -1,28 +1,43 @@
 import React from 'react';
 import Ticket from './Ticket';
 import PropTypes from 'prop-types';
+import { useSelector } from 'react-redux'
+import { useFirestoreConnect, isLoaded, isEmpty } from 'react-redux-firebase'
 
 function TicketList(props) {
-  return (
-    <>
-      <hr/>
-      {Object.values(props.ticketList).map((ticket) =>
-        <Ticket
-          whenTicketClicked={props.onTicketSelection}
-          names={ticket.names}
-          location={ticket.location}
-          issue={ticket.issue}
-          formattedWaitTime={ticket.formattedWaitTime}
-          id={ticket.id}
-          key={ticket.id}
-        />
-      )}
-    </>
-  );
+  useFirestoreConnect([
+    { collection: 'tickets' }
+  ])
+
+  const tickets = useSelector(state => state.firestore.ordered.tickets);
+
+  if (isLoaded(tickets)) {
+    return (
+      <>
+        <hr/>
+        {tickets.map((ticket) => {
+          return <Ticket
+            whenTicketClicked={props.onTicketSelection}
+            names={ticket.names}
+            location={ticket.location}
+            issue={ticket.issue}
+            formattedWaitTime={ticket.formattedWaitTime}
+            id={ticket.id}
+            key={ticket.id}
+          />
+        })}
+      </>
+    );
+  } else {
+    return (
+      <>
+        <h3>Loading...</h3>
+      </>
+    )
+  }
 }
 
 TicketList.propTypes = {
-  ticketList: PropTypes.object,
   onTicketSelection: PropTypes.func
 };
 
