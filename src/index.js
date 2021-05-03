623931ac8e6661f93d794bafe85285c7f51dd0c2
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-05-03 11:07:47 -0700

    Add firebase config and firestore reducer
    Co-authored-by: Joseph Giunta <jgnohack@gmail.com>
    Co-authored-by: Scott O'Neil <scottieoneil@gmail.com>

--- src/index.js
+++ src/index.js
@@ -3,19 +3,28 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import { createStore } from 'redux';
-// import reducer from './reducers/ticket-list-reducer';
 import rootReducer from './reducers/index';
 import { Provider } from 'react-redux';
+import { ReactReduxFirebaseProvider } from 'react-redux-firebase';
+import { createFirestoreInstance } from 'redux-firestore';
+import firebase from "./firebase";
 
 const store = createStore(rootReducer);
 
-// store.subscribe(() =>
-//   console.log(store.getState())
-// );
+const rrfProps = {
+  firebase,
+  config: {
+    userProfile: "users"
+  },
+  dispatch: store.dispatch,
+  createFirestoreInstance
+}
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <ReactReduxFirebaseProvider {...rrfProps}>
+      <App />
+    </ReactReduxFirebaseProvider>
   </Provider>,
   document.getElementById('root')
 );
